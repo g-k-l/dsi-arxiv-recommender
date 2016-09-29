787bbdedddf946113f4714d8005d3117c43a8ff4
g-k-l <gkliu93@msn.com>
2016-09-29 14:35:25 -0700

    added psql connection

--- scrape/harvest.py
+++ scrape/harvest.py
@@ -24,16 +24,26 @@ https://pypi.python.org/pypi/oaiharvest
 
 import os
 from datetime import date, timedelta
+import psycopg2
 
-# start_date = datetime(1992,1,1)
-start_date = date(2016,9,15)
+# Get the database connection
+conn = psycopg2.connect(host='arxivpsql.cctwpem6z3bt.us-east-1.rds.amazonaws.com',
+                        user='root', password='1873', database='arxivpsql')
+cur = conn.cursor()
+cur.execute("CREATE TABLE test (...)")
+
+
+
+start_date = datetime(1992,1,1)
+# start_date = date(2016,9,20)
 end_date = date(2016,9,24)
 output_dir = '../tmp'
 current_date = start_date
 td = timedelta(days=1)
 
 while current_date+td != end_date:
-        get_xml = "oai-harvest --from %s --until %s -d %s http://export.arxiv.org/oai2"%(str(current_date), str(current_date+td), output_dir)
-        os.system(get_xml)
-        push_to_s3 = ""
-        current_date+=td
+    get_xml = "oai-harvest --from %s --until %s -d %s http://export.arxiv.org/oai2"%(str(current_date), str(current_date+td), output_dir)
+    os.system(get_xml)
+    move_to_s3 = "aws s3 mv ../tmp s3://arxiv_meta_data_raw --recursive"
+    os.system(move_to_s3)
+    current_date+=td*10
