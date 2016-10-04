bba55ca0ebc7872d5247df109d294fa338df4a2f
miahunsicker <hunsickermia@gmail.com>
2016-10-04 12:58:35 -0700

    fixed compute sim

--- model/compute_similarity.py
+++ model/compute_similarity.py
@@ -1,22 +1,24 @@
 import os
-import pickle
+import csv
 import numpy as np
 from scipy.spatial.distance import cosine
 from gensim.models.doc2vec import Doc2Vec
 
 '''Compute the similarity of the document vectors.'''
 
+print 'Starting'
+
 model = Doc2Vec.load('adam.first')
 doc_vecs = np.array(Doc2Vec.docvecs())
 n_vecs = len(doc_vecs)
 
-cos_dist_matrix = np.zeroes(shape=(n_vecs, n_vecs))
+print 'Loaded.'
 
-for i, doc_vec in enumerate(doc_vecs):
-    cos_dists = np.apply_along_axis(lambda v: cosine(doc_vec, v), axis=1,arr=doc_vecs)
-    cos_dist_matrix[i, ] = cos_dists
+with open('cos_sim_matrix.csv','w') as f:
+    csv_writer = csv.writer(f)
+    print 'Current iteration: ', i
+    for i, doc_vec in enumerate(doc_vecs):
+        cos_sims = np.apply_along_axis(lambda v: 1-cosine(doc_vec, v), axis=1,arr=doc_vecs)
+    csv_writer.writerow(cos_sims)
 
-cos_sim_matrix = 1 - cos_dist_matrix
-
-with open('cos_sim_matrix.adam.first.pkl', 'wb'):
-    pickle.dump(cos_sim_matrix)
+print 'Job Completed.'
