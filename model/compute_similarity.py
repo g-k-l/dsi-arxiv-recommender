f8cfdf8e6716e7416cd78b1e3a3f070c0c7bcb91
Ubuntu <ubuntu@ip-172-31-62-143.ec2.internal>
2016-10-04 19:42:01 +0000

    compute sim

--- model/compute_similarity.py
+++ model/compute_similarity.py
@@ -7,12 +7,13 @@ from gensim.models.doc2vec import Doc2Vec
 '''Compute the similarity of the document vectors.'''
 
 model = Doc2Vec.load('adam.first')
-doc_vecs = np.array(Doc2Vec.docvecs())
+doc_vecs = np.array(model.docvecs())
 n_vecs = len(doc_vecs)
 
 cos_dist_matrix = np.zeroes(shape=(n_vecs, n_vecs))
 
 for i, doc_vec in enumerate(doc_vecs):
+    print 'iteration ', i
     cos_dists = np.apply_along_axis(lambda v: cosine(doc_vec, v), axis=1,arr=doc_vecs)
     cos_dist_matrix[i, ] = cos_dists
 
