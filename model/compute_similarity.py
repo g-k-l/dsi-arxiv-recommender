61c8128064dae8c3b1e4d9c27ce9e58d85e93fb9
Ubuntu <ubuntu@ip-172-31-51-201.ec2.internal>
2016-10-05 19:52:37 +0000

    updated similarity

--- model/compute_similarity.py
+++ model/compute_similarity.py
@@ -1,26 +1,69 @@
-import os
-import csv
+import os 
+from time import time
 import numpy as np
 from multiprocessing import cpu_count
 from gensim.models.doc2vec import Doc2Vec
 import pyspark as ps
 from pyspark.mllib.linalg.distributed import RowMatrix
-
+import matplotlib.pyplot as plt 
+import seaborn
 '''Compute the similarity of the document vectors.'''
 
-
-def get_row_matrix(sc, model='adam.first'):
+def get_row_matrix( sc, model='adam.first',test=True):
     model = Doc2Vec.load(model)
-    docvecs = np.matrix(model.docvecs).T
+    docvecs = np.array(model.docvecs).T
     n_rows = docvecs.shape[0]
-    mat_rdd = sc.parallelize(docvecs)
+    if not test:
+        mat_rdd = sc.parallelize(docvecs)
+    else:
+        idx_selected = np.random.choice(np.arange(docvecs.shape[1]),size=1000,replace=False)
+        mat_rdd = sc.parallelize(docvecs[:,idx_selected])
     mat = RowMatrix(mat_rdd, n_rows)
-    return mat
+    return mat, idx_selected
 
-def get_col_sim(rowmatrix):
-    return rowmatrix.columnSimilarities()
+def get_col_sim(rowmatrix, compute_threshold): 
+    return rowmatrix.columnSimilarities(compute_threshold)
+    
+def threshold_filter(threshold):
+    def helper(entry):
+        if entry.value > threshold:
+            return True
+        return False
+    return helper 
+
+def get_stats(result):
+    '''result is a list of MatrixEntries'''
+    values = map(lambda entry: entry.value, result)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_xlabel = 'Cosine Similarities'
+    ax.set_ylabel = 'Vectors'
+    ax.hist(values, bins=20)
+    plt.savefig('Similarity Historgram.jpg')
+    print 'Average: ', sum(values)/len(values)
+    print 'Num values: ', len(values)
+
+    print 'Starting'
+    sc = ps.SparkContext('local[{}]'.format(cpu_count()))
+    get_stats(one_iter(sc))
+
+def one_iter(sc):
+    time_0 = time()
+    rowmat, idx_selected = get_row_matrix(sc)
+    col_sims_mat = get_col_sim(rowmat, 0)
+    time_1 = time()
+    col_sims = col_sims_mat.entries.filter(threshold_filter(0))
+    time_2 = time()
+    result = col_sims.collect()
+    time_3 = time()
+   
+    print 'Working time to get col_sims_mat: ', time_1-time_0
+    print 'Working time to filter: ', time_2-time_0
+    print 'Working time to collect entries: ', time_3-time_0
+    return result, idx_selected
 
 if __name__ == '__main__':
     print 'Starting'
     sc = ps.SparkContext('local[{}]'.format(cpu_count()))
-    col_sims = get_col_sim(get_row_matrix(sc))
+    result,idx_selected=one_iter(sc)
+    get_stats(result)
