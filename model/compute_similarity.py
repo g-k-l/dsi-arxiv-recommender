cc746284750b72072c4ed1fa679c6d696bd49a66
miahunsicker <hunsickermia@gmail.com>
2016-10-04 18:20:14 -0700

    blah

--- model/compute_similarity.py
+++ model/compute_similarity.py
@@ -1,24 +1,26 @@
 import os
 import csv
 import numpy as np
-from scipy.spatial.distance import cosine
+from multiprocessing import cpu_count
 from gensim.models.doc2vec import Doc2Vec
+import pyspark as ps
+from pyspark.mllib.linalg.distributed import RowMatrix
 
 '''Compute the similarity of the document vectors.'''
 
-print 'Starting'
 
-model = Doc2Vec.load('adam.first')
-doc_vecs = np.array(Doc2Vec.docvecs
-n_vecs = len(doc_vecs)
+def get_row_matrix(sc, model='adam.first'):
+    model = Doc2Vec.load(model)
+    docvecs = np.matrix(model.docvecs).T
+    n_rows = docvecs.shape[0]
+    mat_rdd = sc.parallelize(docvecs)
+    mat = RowMatrix(mat_rdd, n_rows)
+    return mat
 
-print 'Loaded.'
+def get_col_sim(rowmatrix):
+    return rowmatrix.columnSimilarities()
 
-with open('cos_sim_matrix.csv','w') as f:
-    csv_writer = csv.writer(f)
-    print 'Current iteration: ', i
-    for i, doc_vec in enumerate(doc_vecs):
-        cos_sims = np.apply_along_axis(lambda v: 1-cosine(doc_vec, v), axis=1,arr=doc_vecs)
-    csv_writer.writerow(cos_sims)
-
-print 'Job Completed.'
+if __name__ == '__main__':
+    print 'Starting'
+    sc = ps.SparkContext('local[{}]'.format(cpu_count()))
+    col_sims = get_col_sim(get_row_matrix(sc))
