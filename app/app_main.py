f6f8cbb48626c733064d17f893e9eecc479841e3
g-k-l <gkliu93@msn.com>
2016-10-14 14:28:23 -0700

    working on webapp

--- app/app_main.py
+++ app/app_main.py
@@ -1,22 +1,32 @@
-from flask import Flask, render_template, send_from_directory, jsonify
+import pandas as pd
+from flask import Flask
+from flask import render_template, request
 from data_stats import data_master as dm
 
-# app = Flask(__name__, static_url_path='')
-app = Flask(__name__)
+#Load the model and other infrastructure here
+subjects_info= pd.read_csv('./data_stats/count_by_subject.txt')
+subjects_info.columns = ['subject','subject_id','count']
+subjects_info.sort(columns='subject',inplace=True)
 
+app = Flask(__name__)
 @app.route("/")
+@app.route("/index.html")
 def index():
-    return render_template('index.html')
-#
-# @app.route("/data")
-# def data():
-#     return str(dm.get_data())
-#
-# @app.route("/display")
-# def display():
-#     return send_from_directory('','index.html')
+    #Run the model here to get some result.
+    #Pass the result to Jinja template in render_template
+    return render_template('index.html',subjects=subjects_info['subject'].tolist())
+
+@app.route("/result.html", methods=['POST'])
+def result():
+    value_list = request.form.getlist('select[]')
+    too_many = len(value_list)>2
+    result_list=[]
+    return render_template('result.html', result=result_list)
+
+@app.route("/about.html")
+def about():
+    return render_template('about.html')
+
 
 if __name__ == '__main__':
-    app.run()
-    # url_for('static','index.html')
-    # url_for('static','bubble.html')
+    app.run(debug=True)
