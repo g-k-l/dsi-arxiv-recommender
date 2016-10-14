97c7159692515e47f715e307da70d86f1278c498
g-k-l <gkliu93@msn.com>
2016-10-13 21:50:35 -0700

    added bin by subject id

--- model/main_model.py
+++ model/main_model.py
@@ -1,7 +1,17 @@
 import pickle
+import csv
+from collections import defaultdict
 from scipy.spatial.distance import cosine
 from gensim.models.doc2vec import Doc2Vec
 from multiprocessing import Pool, cpu_count
+
+'''
+This module builds the main_model, which is a dictionary of the form:
+
+{ subject_id: { arxiv_id: { subject_id: cos_sim \} } }
+
+'''
+
 
 def build_subject_model(model):
     '''
@@ -54,7 +64,34 @@ def build_single_doc_dict(idx, doc_vec, subject_centroids):
     return idx, subject_doc_sims
 
 
+def bin_by_subject_id(model_dict):
+    '''
+    Builds the following dictionary:
+    { subject_id: list of \{arxiv_id: \{ subject_id: cos_sims \}\}}
+    the list contains the similarities of the articles with the cosine sims, organized
+    by subject_id for easy lookup.
+    '''
+    result_d = {}
+    with open('./assets/subject_id_arxiv_id.csv') as f:
+        csv_file = csv.reader(f)
+        for i, line in enumerate(csv_file):
+            if i % 10000==0:
+                print 'Currently on iteration: ', i
+            subject_id, arxiv_id = int(line[0]), line[1]
+            if subject_id in result_d:
+                result_d[subject_id][arxiv_id] = model_dict[arxiv_id]
+            else:
+                result_d[subject_id] = {arxiv_id: model_dict[arxiv_id]}
+
+    with open('./assets/complete_model_test.pkl', 'wb') as f:
+        pickle.dump(result_d, f)
+
+    return result_d
+
+
 if __name__ == '__main__':
-    model = Doc2Vec.load('./assets/second_model/second_model')
-    model_dict = build_subject_model(model)
-    
+    # model = Doc2Vec.load('./assets/second_model/second_model')
+    # model_dict = build_subject_model(model)
+    with open('./assets/subject_model_test.pkl','wb') as f:
+        model_dict_test = pickle.load(f)
+    bin_by_subject_id(model_dict_test)
