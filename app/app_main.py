8f8e1960df4c4f4c444b2e7a1c20942d58f36aa7
g-k-l <gkliu93@msn.com>
2016-10-13 23:58:55 -0700

    added app resources

--- app/app_main.py
+++ app/app_main.py
@@ -1,17 +1,22 @@
 from flask import Flask, render_template, send_from_directory, jsonify
 from data_stats import data_master as dm
 
-app = Flask(__name__, static_url_path='')
+# app = Flask(__name__, static_url_path='')
+app = Flask(__name__)
 
-@app.route("/data")
-def data():
-    return str(dm.get_data())
-
-@app.route("/display")
-def display():
-    return send_from_directory('','index.html')
+@app.route("/")
+def index():
+    return render_template('index.html')
+#
+# @app.route("/data")
+# def data():
+#     return str(dm.get_data())
+#
+# @app.route("/display")
+# def display():
+#     return send_from_directory('','index.html')
 
 if __name__ == '__main__':
     app.run()
-    url_for('static','index.html')
-    url_for('static','bubble.html')
+    # url_for('static','index.html')
+    # url_for('static','bubble.html')
