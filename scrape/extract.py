15fb05f55911cf63d99f54b4b00faf9bd1fdaf5d
Kevin Liu <gkliu93@msn.com>
2019-09-28 07:17:45 -0700

    move xml_parser to scrape and add tests

--- scrape/extract.py
+++ scrape/extract.py
@@ -1,25 +1,31 @@
 # -*- coding: utf-8 -*-
-from xml.etree import ElementTree as ET
-from datetime import datetime
-
 """
-This module is ported from arXiv-doc2vec-recommender with some slight modifications.
+This module is ported from arXiv-doc2vec-recommender
 Credits go to https://github.com/sepehr125
 """
 
-root_tag = '{http://purl.org/dc/elements/1.1/}'
+from xml.etree import ElementTree as ET
+from datetime import datetime
+
+
+XMLNS = '{http://purl.org/dc/elements/1.1/}'
+
 
 def get_fields(body):
     """
     Args:
         body (str): the body of the file
-
     Returns:
         tuple: (title, authors, subject, abstract, last_submitted, arxiv_id)
-        bool: False if file is not xml
     """
     root = ET.fromstring(body)
-    return (get_title(root), get_authors(root), get_subject(root), get_abstract(root), get_date(root), get_arxivid(root))
+    return (get_title(root),
+            get_authors(root),
+            get_subject(root),
+            get_abstract(root),
+            get_date(root),
+            get_arxivid(root))
+
 
 '''
 INPUT:
@@ -30,25 +36,27 @@ OUTPUT:
     None: if no identifier looking like an arxiv URL exists
 '''
 
+
 def get_title(root):
-    tag = '{http://purl.org/dc/elements/1.1/}title'
+    tag = XMLNS + "title"
     title = root.find(tag)
     if title is not None:
         return title.text
 
+
 def get_authors(root):
     """OUTPUT: List of string of authors"""
-    tag = '{http://purl.org/dc/elements/1.1/}creator'
+    tag = XMLNS + "creator"
     authors = root.findall(tag)
     if authors:
-        authors = [el.text for el in authors]
-        return authors
+        return [el.text for el in authors]
+
 
 def get_subject(root):
-    tag = '{http://purl.org/dc/elements/1.1/}subject'
-    subject = root.find(tag)
-    if type(subject) == ET.Element:
-        return subject.text
+    tag = XMLNS + "subject"
+    subjects = root.findall(tag)
+    if subjects:
+        return [sub.text for sub in subjects]
 
 
 def get_abstract(root):
@@ -56,7 +64,7 @@ def get_abstract(root):
     There are two elements with the `description` tag name.
     The longer one is the abstract.
     """
-    tag = '{http://purl.org/dc/elements/1.1/}description'
+    tag = XMLNS + "description"
     descriptions = root.findall(tag)
     if descriptions:
         abstract = max([el.text for el in descriptions], key=len)
@@ -65,23 +73,27 @@ def get_abstract(root):
 
 def get_arxivid(root):
     """
-    The arxiv id is hidden among several fields all with the tag name "identifier".
-    Fortunately, the arxiv id is the full URL at arxiv.org for the abstract,
-    so we can identify them checking if they contain 'arxiv.org'
+    The arxiv id is hidden among several fields all with the
+    tag name "identifier". Fortunately, the arxiv id is the full
+    URL at arxiv.org for the abstract, so we can identify them
+    checking if they contain 'arxiv.org'
     """
-    tag = '{http://purl.org/dc/elements/1.1/}identifier'
+    tag = XMLNS + "identifier"
     ids = root.findall(tag)
     if ids:
         for el in ids:
             if el.text.startswith('http://arxiv.org/abs/'):
                 return el.text
 
+
 def get_date(root):
     """
-    OUTPUT: datetime: last submitted date in Y-m-d format
+    OUTPUT: datetime: last submitted date in %Y-%m-%d format
     """
-    tag = '{http://purl.org/dc/elements/1.1/}date'
+    tag = XMLNS + "date"
     date_list = root.findall(tag)
     if date_list:
-        dates = [datetime.strptime(el.text, "%Y-%m-%d").date() for el in date_list]
+        dates = [datetime.strptime(el.text, "%Y-%m-%d").date()
+                 for el in date_list]
         return (max(dates))
+
---
+++ scrape/tests.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from unittest import TestCase
+
+from extract import get_fields
+
+
+class TestExtract(TestCase):
+    def setUp(self):
+        self.f = open("example.xml", "r")
+        self.xmlstr = self.f.read()
+
+    def tearDown(self):
+        self.f.close()
+
+    def test_get_fields(self):
+        actual = get_fields(self.xmlstr)
+        self.assertEqual(actual, (
+            "A POVM view of the ensemble approach to polarization optics",
+            ["Sudha", "Rao, A. V. Gopala", "Devi, A. R. Usha",
+             "Rajagopal, A. K."],
+            ["Physics - Optics", "Physics - Classical Physics"],
+            "redacted, redacted, redacted, redacted, redacted",
+            datetime.strptime("2007-06-20", "%Y-%m-%d").date(),
+            "http://arxiv.org/abs/0704.0147",
+        ))
+
